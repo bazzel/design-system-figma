0f59096cb0727e08eed5d035d3bc2e62cb18b87e
bazzel <patrick.baselier@gmail.com>
2024-10-06 21:53:07 +0200

    Add high voltage gem

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
-}
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js",
+    "./app/{components,views}/**/*.{erb,haml,html,slim,rb}",
+  ],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
